448fce344d19fbc10d310195e0aa43349b6b8982
우성원 <frosttvinkorea@gmail.com>
2023-02-02 22:10:16 +0900

    게임 연출 - 공포 카메라
    제한적인 마우스 움직임으로 2D 게임에서 답답함과 공포감을 체험할 수 있는 연출입니다.

---
+++ scary.py
@@ -0,0 +1,38 @@
+import pygame
+import random
+
+SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 1300
+BLACK = (0, 0, 0)
+FRAME = pygame.image.load("CAMERA_FRAME.png")
+BACKGROUND = pygame.image.load("hospital.png")
+
+
+def mouse_movement(screen, mouseX, mouseY):
+    a = random.randint(1, 20)
+    b = random.randint(1, 20)
+    x = (mouseX - 650 + a)/20
+    y = (mouseY - 350 + b)/20
+    screen.blit(BACKGROUND, [-150 + x, -150 + y])
+    screen.blit(FRAME, [0, 0])
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("HOSPITAL")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    pygame.mixer.music.load("cave.wav")
+    pygame.mixer.music.play(-1)
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        pygame.mouse.set_visible(False)
+        mouseX, mouseY = pygame.mouse.get_pos()
+        mouse_movement(screen, mouseX, mouseY)
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
