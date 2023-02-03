00ddd9082b995a09824ef18a342137bd2c9b257e
우성원 <frosttvinkorea@gmail.com>
2023-02-03 22:59:11 +0900

    마우스 반전 수정
    마우스 이동효과가 반전되어 수정합니다.

--- scary.py
+++ scary.py
@@ -4,6 +4,8 @@ import random
 SCREEN_HEIGHT = 700
 SCREEN_WIDTH = 1300
 BLACK = (0, 0, 0)
+fps = 0
+maxframe = 60
 FRAME = pygame.image.load("CAMERA_FRAME.png")
 BACKGROUND = pygame.image.load("hospital.png")
 
@@ -11,8 +13,8 @@ BACKGROUND = pygame.image.load("hospital.png")
 def mouse_movement(screen, mouseX, mouseY):
     a = random.randint(1, 20)
     b = random.randint(1, 20)
-    x = (mouseX - 650 + a)/20
-    y = (mouseY - 350 + b)/20
+    x = (mouseX - 650 + a)/20*-1
+    y = (mouseY - 350 + b)/20*-1
     screen.blit(BACKGROUND, [-150 + x, -150 + y])
     screen.blit(FRAME, [0, 0])
 
@@ -32,7 +34,7 @@ def main():
         mouseX, mouseY = pygame.mouse.get_pos()
         mouse_movement(screen, mouseX, mouseY)
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(maxframe)
 
 if __name__ == '__main__':
     main()
